0532b4cd2e9ff1432a5268002a73b07d8cd5f3f7
kyle-judd <juddkyle1408@gmail.com>
2020-02-28 17:35:46 -0500

    created contact form jsp file

--- src/main/java/com/kylejudd/football/controllers/ContactController.java
+++ src/main/java/com/kylejudd/football/controllers/ContactController.java
@@ -1,5 +1,15 @@
 package com.kylejudd.football.controllers;
 
-public class ContactController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/contact")
+public class ContactController {
+	
+	@GetMapping("/contactForm")
+	public String contactForm() {
+		return "contact-form";
+	}
 }
